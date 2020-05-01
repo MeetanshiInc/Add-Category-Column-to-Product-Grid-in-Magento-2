1d653abd6b7e0bd0417008bf4a76b72980e388d7
Dhara Tuvar <48232912+dharatuvar@users.noreply.github.com>
2020-05-01 16:46:07 +0530

    Add Category Column to Product Grid in Magento 2

---
+++ Model/Category/Categorylist.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Meetanshi\GridColumn\Model\Category;
+
+class CategoryList implements \Magento\Framework\Option\ArrayInterface
+{
+    public function __construct(
+        \Magento\Catalog\Model\ResourceModel\Category\CollectionFactory $collectionFactory
+    ) {
+        $this->_categoryCollectionFactory = $collectionFactory;
+
+    }
+    public function toOptionArray($addEmpty = true)
+    {
+
+         $collection = $this->_categoryCollectionFactory->create();
+        $collection->addAttributeToSelect('name');//->addRootLevelFilter()->load();
+        $options = [];
+        if ($addEmpty) {
+            $options[] = ['label' => __('-- Please Select a Category --'), 'value' => ''];
+        }
+        foreach ($collection as $category) {
+            $options[] = ['label' => $category->getName(), 'value' => $category->getId()];
+        }
+        return $options;
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# How to Add Category Column to Product Grid in Magento 2
+
+Grids in Magento 2 are an easy way to list, sort, filter, edit, or update data. Here's the solution to ***add category column to product grid in Magento 2***.
+
+The default Magento 2 product grid allows adding columns of the product attribute. However, adding custom columns of category in the product grid in Magento 2 is not easy.
+
+You can use this solution to add category column which can be useful for better product management and optimized backend performance.
+
+Follow the detailed guide to [Add Category Column to Product Grid in Magento 2](https://meetanshi.com/blog/add-category-column-to-product-grid-in-magento-2)
+
